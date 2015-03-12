f739b38d0db3125e3e56e02989b01cca85e473a7
balazs <vargabalazslaszlo@gmail.com>
2015-03-12 16:24:03 +0100

    1_FAILED_class_not_exists

---
+++ test/StringToArrayTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: balimaci
+ * Date: 2015.03.12.
+ * Time: 16:17
+ */
+
+namespace Tdd\Test;
+
+
+class StringToArrayTest extends \PHPUnit_Framework_TestCase
+{
+    public function testStringToArrayExists()
+    {
+        $stringToArray = new StringToArray("a,b,c");
+
+        $this->assertEquals(get_class($stringToArray), 'StringToArray');
+    }
+}
